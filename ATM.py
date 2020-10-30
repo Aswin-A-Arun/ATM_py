eb1e513b8ae6855db543d39f7be73f20cc98986c
Enygma <57713995+Aswin-A-Arun@users.noreply.github.com>
2020-10-30 15:26:30 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,19 @@
+class ATM(object):
+   def __init__(CashWithdrawal,BankDeposit,CashDeposit,BankBalance):
+       self.CashWithdrawal = color
+       self.BankDeposit = BankDeposit
+       self.CashDeposit = CashDeposit
+       self.BankBalance = BankBalance
+
+    def CardInsert(self):
+        print("Insert card")
+
+    def CardPin(self):
+        print("Pin Inputted")
+
+    def AmountWithdrawn/Deposited(self):
+        print("Amount Withdrawn/Deposited")
+        "accelerator functionality here"
+
+    def Balance(self,gear_type):
+        print("Balance Info:")
